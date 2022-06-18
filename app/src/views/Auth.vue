6a47d1a48aaf28d9cbc86f6bb055eb574854ff42
NipunEAmarasekara <namarasekara@typefi.com>
2022-06-18 21:39:06 +0530

    small change added

--- app/src/views/Auth.vue
+++ app/src/views/Auth.vue
@@ -17,7 +17,7 @@ export default {
   mounted: async function () {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
-    console.log(params);
+    console.log(`${window.location.host}/.netlify/functions/auth/v1/authorize?client_id=${params.client_id}&redirect_uri=${params.redirect_uri}&response_type=${params.response_type}&state=${params.state}&user=${this.user.sub}`);
     axios.get(`${window.location.host}/.netlify/functions/auth/v1/authorize?client_id=${params.client_id}&redirect_uri=${params.redirect_uri}&response_type=${params.response_type}&state=${params.state}&user=${this.user.sub}`).then(response => {
       console.log(response);
     });
