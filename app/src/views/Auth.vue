98e83d97ef0f45b45bf21a2a81873e1d1644d735
NipunEAmarasekara <namarasekara@typefi.com>
2022-06-18 21:47:24 +0530

    url updated

--- app/src/views/Auth.vue
+++ app/src/views/Auth.vue
@@ -17,9 +17,8 @@ export default {
   mounted: async function () {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
-    console.log(`https://${window.location.host}/.netlify/functions/auth/v1/authorize?client_id=${params.client_id}&redirect_uri=${params.redirect_uri}&response_type=${params.response_type}&state=${params.state}&user=${this.user.sub}`);
     axios.get(`https://${window.location.host}/.netlify/functions/auth/v1/authorize?client_id=${params.client_id}&redirect_uri=${params.redirect_uri}&response_type=${params.response_type}&state=${params.state}&user=${this.user.sub}`).then(response => {
-      console.log(response);
+      location.href=`${params.redirect_uri}?code=${response.data.data.code}`;
     });
   },
 };
